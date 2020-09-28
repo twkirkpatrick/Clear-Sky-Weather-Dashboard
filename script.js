aa68dc0d00be6b783ab53232e0b85b30cabcafe2
Tanner Kirkpatrick <twk4491@gmail.com>
2020-09-28 14:22:05 -0400

    cleaned up code and added comments

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
 
     var APIKey = "6c280cb21fdeb9b935ffbeed4ff11c37";
-    // var city = "";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
     var units = "&units=imperial&appid=";
     var lat = "";
     var lon = "";
     var secondURL = "https://api.openweathermap.org/data/2.5/onecall?";
     var oneCall = "";
-    var date = "";
-
-
-
+    
+    //on search, current weather div empties and runQuery function fires. Value of input field passed as paramater.
     $("#search-btn").on("click", function () {
 
         $("#current-weather").empty();
@@ -22,13 +19,15 @@ $(document).ready(function () {
 
     });
 
+    //When search history button is clicked, runQuery function is fired; Pass in text of button as parameter.
+
     $(document).on("click", ".cityHistory", function () {
 
         runQuery($(this).text())
     })
 
 
-
+    //Using the data from oneCall API call, populate information using dynamically created HTML.
     function populateForecast(oneCallData) {
        
         var alteredArr = oneCallData.daily;
@@ -42,16 +41,11 @@ $(document).ready(function () {
         for (var i = 0; i < alteredArr.length; i++) {
 
             var block = $("<div>");
-
             block.attr("class", "col");
 
             var icon = alteredArr[i].weather[0].icon;
-            console.log(icon);
-
             var iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
-
             var iconAppend = $("<img>").attr("src", iconURL);
-
             iconAppend.addClass("forecast-img");
 
             var temp = $("<p>").text("Temperature: " + alteredArr[i].temp.day + " °F");
@@ -61,7 +55,6 @@ $(document).ready(function () {
             var timeStamp = alteredArr[i].dt;
 
             var date = moment(timeStamp * 1000).format("dddd");
-
 
             block.append(date + "<br>");
             block.append(iconAppend);
@@ -74,12 +67,13 @@ $(document).ready(function () {
 
     };
 
-    
-
+    //This function dynamically creates list items for searched cities.
     function makeRow(str) {
         var li = $("<li>").addClass("cityHistory").text(str)
         $("#history").append(li)
     }
+
+    //When the clear history button is clicked, the cities are removed local storage and list items disappear.
 
     $("#clearHistory").on("click", function(){
 
@@ -87,13 +81,9 @@ $(document).ready(function () {
 
         $("#history").empty();
 
-
     });
 
-
-
-
-
+    //This function takes in the first API call object, and uses the information to dynamically create the current weather section.
     function secondCall(currentData) {
 
         $.ajax({
@@ -104,21 +94,28 @@ $(document).ready(function () {
             console.log(oneCall);
 
             var cityName = $("<h2>" + currentData.name + "</h2>");
+            
             var iconCode = oneCallData.current.weather[0].icon;
             var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
             var iconAppend = $("<img>").attr("src", iconURL);
+            
             var date = moment().format("MMMM Do[,] YYYY");
             var dateAppend = $("<h5>").text(date);
             dateAppend.append(iconAppend);
+            
             var temp = oneCallData.current.temp;
             var tempDisplay = $("<p>").text("Temperature: " + temp + " °F");
+            
             var humid = oneCallData.current.humidity;
             var humidDisplay = $("<p>").text("Humidity: " + humid + "%");
+           
             var wind = oneCallData.current.wind_speed;
             var windDisplay = $("<p>").text("Wind Speed: " + wind + " MPH");
-            var uv = oneCallData.current.uvi;
             
+            var uv = oneCallData.current.uvi;
             var uvDisplay = $("<p>" + "UV Index: " + uv + "</p>");
+
+            //conditionals change the color of the UV index based on severity.
 
             if (uv > 5 && uv < 8) {
                 uvDisplay.css("color", "orange");
@@ -144,11 +141,10 @@ $(document).ready(function () {
         })
 
     }
-
+    //Initial API call which grabs the latitude and longitude of the city, making it possible to execute the second call.
     function runQuery(city) {
 
         var search = queryURL + city + units + APIKey;
-        console.log(queryURL);
         $("#current-weather").empty();
 
         $.ajax({
@@ -156,7 +152,7 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (currentData) {
 
-
+            //setting the cities to local storage and executes makeRow function.
             if (history.indexOf(city) === -1) {
                 history.push(city);
                 localStorage.setItem("history", JSON.stringify(history));
